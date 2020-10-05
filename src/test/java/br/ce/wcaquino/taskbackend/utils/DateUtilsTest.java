c8f8bcf195f5424b158bc78bf81eaba3deaecf58
Ronald Abrantes Menezes <ronaldabrantes.menezes@altran.com>
2020-10-05 16:10:56 +0100

    Added unit test coverage to TaskController

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -6,23 +6,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class DateUtilsTest {
+    @Test
+    public void mustReturnTrueToFutureDates() {
+        LocalDate localDate = LocalDate.now().plusDays(1);
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(localDate));
+    }
 
-	@Test
-	public void mustReturnTrueToFutureDates() {
-		LocalDate localDate = LocalDate.now().plusDays(1);
-		Assert.assertTrue(DateUtils.isEqualOrFutureDate(localDate));
-	}
+    @Test
+    public void mustReturnFalseToFutureDates() {
+        LocalDate localDate = LocalDate.now().plusDays(-1);
+        Assert.assertFalse(DateUtils.isEqualOrFutureDate(localDate));
+    }
 
-	@Test
-	public void mustReturnFalseToFutureDates() {
-		LocalDate localDate = LocalDate.now().plusDays(-1);
-		Assert.assertFalse(DateUtils.isEqualOrFutureDate(localDate));
-	}
-
-	@Test
-	public void mustReturnTrueToPresentDates() {
-		LocalDate localDate = LocalDate.now();
-		Assert.assertTrue(DateUtils.isEqualOrFutureDate(localDate));
-	}
+    @Test
+    public void mustReturnTrueToPresentDates() {
+        LocalDate localDate = LocalDate.now();
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(localDate));
+    }
 
 }
