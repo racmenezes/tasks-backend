4bee53538a314de0cf6556972d628a8231b64ffd
Ronald Abrantes Menezes <ronaldabrantes.menezes@altran.com>
2020-10-05 16:38:35 +0100

    Refactore to test build Jenkins trigger

--- src/main/java/br/ce/wcaquino/taskbackend/utils/DateUtils.java
+++ src/main/java/br/ce/wcaquino/taskbackend/utils/DateUtils.java
@@ -4,7 +4,8 @@ import java.time.LocalDate;
 
 public class DateUtils {
 
-	public static boolean isEqualOrFutureDate(LocalDate date) {
-		return date.isEqual(LocalDate.now()) || date.isAfter(LocalDate.now());
-	}
+    public static boolean isEqualOrFutureDate(LocalDate date) {
+        return date.isEqual(LocalDate.now()) || date.isAfter(LocalDate.now());
+    }
+
 }
