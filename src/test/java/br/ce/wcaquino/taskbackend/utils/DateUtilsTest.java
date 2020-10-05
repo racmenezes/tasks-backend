6a53162c35f920d2c24df4b7da00531825ce0ca7
Ronald Abrantes Menezes <ronaldabrantes.menezes@altran.com>
2020-10-05 13:16:31 +0100

    Added unit test coverage to DateUtils.isEqualOrFutureDate function

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -0,0 +1,28 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DateUtilsTest {
+
+	@Test
+	public void mustReturnTrueToFutureDates() {
+		LocalDate localDate = LocalDate.now().plusDays(1);
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(localDate));
+	}
+
+	@Test
+	public void mustReturnFalseToFutureDates() {
+		LocalDate localDate = LocalDate.now().plusDays(-1);
+		Assert.assertFalse(DateUtils.isEqualOrFutureDate(localDate));
+	}
+
+	@Test
+	public void mustReturnTrueToPresentDates() {
+		LocalDate localDate = LocalDate.now();
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(localDate));
+	}
+
+}
